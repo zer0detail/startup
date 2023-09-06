8fd610f390a3b51c0f427647a990b16a8f600d69
Zerodetail <zerodetail@gmail.com>
2023-09-06 21:51:27 +1000

    fix nvim transparent background

--- nvim/lua/zero/colourscheme.lua
+++ nvim/lua/zero/colourscheme.lua
@@ -1,5 +1,9 @@
 local colour = "catppuccin"
 
+require("catppuccin").setup({
+  transparent_background = true
+})
+
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colour)
 if not status_ok then
   vim.notify("colorscheme " .. colour .. " not found!")
